acbabe8bcd068819ad620953cee9ba5fa22e8aa0
rhard32 <rhard32@gmail.com>
2015-04-09 15:19:51 -0400

    Initial commit

---
+++ BookService/Models/BookServiceContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace BookService.Models
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
+
+namespace BookService.Models
+{
+    public class BookDetailDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+    }
+}
+
+namespace BookService.Models
+{
+    public class BookServiceContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public BookServiceContext() : base("name=BookServiceContext")
+        {
+
+
+    // New code:
+    this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
+
+        }
+
+        public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<BookService.Models.Book> Books { get; set; }
+    
+    }
+}
