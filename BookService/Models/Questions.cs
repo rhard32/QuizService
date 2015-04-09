177b83a77234ffeb30436f3cf73343bf3a4a1b45
rhard32 <rhard32@gmail.com>
2015-04-09 16:50:59 -0400

    adding text to question model

--- BookService/Models/Questions.cs
+++ BookService/Models/Questions.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
-namespace QuizService.Models
+namespace BookService.Models
 {
     public class Questions
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Question { get; set; }
     }
 }
